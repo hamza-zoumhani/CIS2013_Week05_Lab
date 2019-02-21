18a29c262d041015989691898953d6de282b2872
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-20 18:52:22 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,18 +4,18 @@ using namespace std;
 
 int main(){
 	
-	const int len=3;
+	const int LEN=3;
 	
 	// List of student in our class:
-	int student[len];
+	int student[LEN];
 	
 	
-	for(int i=0; i<len; i++){
+	for(int i=0; i<LEN; i++){
 		cout << "Enter Student " << i+1 << "'s grade: ";
 		cin >> student[i];
 	}
 	
-	for(int j=0; j<len; j++){
+	for(int j=0; j<LEN; j++){
 		cout << "Student " << j+1 << "'s grade is " << student[j] << endl;
 		
 	}
