b1b38777426080847ea70d9fa90a6288cb746be7
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-20 19:25:39 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,27 +2,37 @@
 #include <string>
 using namespace std;
 
+const int MAX=30;
+int student[MAX];
+string name[MAX];
+
+void makesStudent(int x){
+	cout << "Enter Student " << x+1 << "'s name: ";
+	cin >> name[x];
+	cout << "Enter " << name[x] << "'s grade: ";
+	cin >> student[x];
+	cout << endl;
+}
+
+void  printGrade(int y){
+	cout << name[y] << "'s grade is " << student[y] << endl;
+}
+
 int main(){
 	
-	const int MAX=30;
 	int LEN;
 	
 	cout << "How many students are in this class: " ;
 	cin >> LEN;
-	
-	int student[MAX];
-	string name[MAX];
+	cout << endl;
 	
 	
 	for(int i=0; i<LEN; i++){
-		cout << "Enter Student " << i+1 << "'s name: ";
-		cin >> name[i];
-		cout << "Enter " << name[i] << "'s grade: ";
-		cin >> student[i];
+		makesStudent(i);
 	}
 	
 	for(int j=0; j<LEN; j++){
-	cout << name[j] << "'s grade is " << student[j] << endl;
+		printGrade(j);
 		
 	}
 	
