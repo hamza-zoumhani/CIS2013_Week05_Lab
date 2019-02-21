7311faedb201e1f634debcc8aa5ee0d2776c0b45
Hamza Zoumhani <hamza.zoumhani@okstate.edu>
2019-02-20 18:14:50 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	
+	int bob =90;
+	int tom= 15;
+	int jill= 99;
+	
+	cout << "Enter Bobs grade: " ;
+	cin >>bob;
+	cout << "Enter Toms grade: " ;
+	cin >>tom;
+	cout << "Enter Toms grade: " ;
+	cin >>jill;
+	
+	cout << "Bob gramde is: " << bob << endl;
+	cout << "Tom grade is: " << tom << endl;
+	cout << "Jill grade is: " << jill << endl;
+	
+	return 0;
+}
